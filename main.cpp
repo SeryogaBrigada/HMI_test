4e3c3b91c96597ca1fc8a10bf25710619ea68064
SeryogaBrigada <seryoga.engineering@gmail.com>
2018-08-15 16:50:34 +0300

    First release

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+#include <aktualizr/src/libaktualizr/primary/aktualizr.h>
+
+int main(int argc, char *argv[])
+{
+    Config m_conf("/home/intel/OTA-HMI/HMI_test/aktualizr/config/sota_local.toml");
+    Aktualizr m_art(m_conf);
+
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
+
+    return app.exec();
+}
